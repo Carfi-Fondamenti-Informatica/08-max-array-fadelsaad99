ffca66f129f1fa9766ecc7a05d1f6d848c69fdc1
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-12-11 14:34:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int dim;
+    cin>>dim;
+    float arr[dim];
+    float x;
+    for(int i=0; i<dim; i++){
+        cin>>x;
+        arr[i]=x;
+    }
+    cout<<maggio(arr,dim);
+
+    return 0;
 }
