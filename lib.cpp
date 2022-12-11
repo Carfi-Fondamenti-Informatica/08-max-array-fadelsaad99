b4c0c0b5dee544a7048e6eeb9ab42f6d1031b6ee
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-12-11 14:33:40 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+#include "lib.h"
+float maggio(float arr[], int dim) {
+    float x=1;
+    for (int k=0; k<dim; k++){
+        if(arr[k]>x){
+            x=arr[k];
+        }
+    }
+    return x;
+}
