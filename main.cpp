656bf19da978e7ed18da7723bec9edbdbeb08f89
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-12-11 14:45:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main() {
-    int m;
-    cin>>m;
-    float arr[m];
-    float p;
-    for(int i=0; i<m; i++){
-        cin>>p;
-        arr[i]=p;
+    int h;
+    cin>>h;
+    float d[h];
+    float x;
+    for(int i=0; i<h; i++){
+        cin>>x;
+        d[i]=x;
     }
-    cout<<ilmaggio(arr,m);
+    cout<<max(d,h);
 
     return 0;
 }
